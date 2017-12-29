c0a8be2630ca1fe4b2484b54c2d181b805065107
HY-ZhengWei <hy.zhengwei@qq.com>
2017-12-29 16:35:09 +0800

    1.0.1
    Signed-off-by: HY-ZhengWei <hy.zhengwei@qq.com>

--- Common.003.Win.UI.bat
+++ Common.003.Win.UI.bat
@@ -4,7 +4,7 @@ cd .\bin
 
 rd /s/q .\org\hy\common\ui\junit
 
-jar cvfm hy.common.ui.jar MANIFEST.MF LICENSE org
+jar cvfm hy.common.ui.jar MANIFEST.MF META-INF org
 
 copy hy.common.ui.jar ..
 del /q hy.common.ui.jar
--- Common.004.Mac.UI.sh
+++ Common.004.Mac.UI.sh
@@ -4,7 +4,7 @@ cd ./bin
 
 rm -R ./org/hy/common/ui/junit
 
-jar cvfm hy.common.ui.jar MANIFEST.MF LICENSE org
+jar cvfm hy.common.ui.jar MANIFEST.MF META-INF org
 
 cp hy.common.ui.jar ..
 rm hy.common.ui.jar
