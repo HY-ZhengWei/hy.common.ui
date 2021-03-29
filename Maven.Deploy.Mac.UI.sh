bfe9958edb662e05194da2faa279fb30eeb5560b
HY-ZhengWei <HY.ZhengWei@qq.com>
2021-03-29 14:54:22 +0800

    1.0.1
    1. 添加：Gitee.com 代码托管

--- Maven.Deploy.Mac.UI.sh
+++ Maven.Deploy.Mac.UI.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mvn deploy:deploy-file -Dfile=hy.common.ui.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/ui/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:9015/nexus/content/repositories/thirdparty
-mvn deploy:deploy-file -Dfile=hy.common.ui-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/ui/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:9015/nexus/content/repositories/thirdparty
+mvn deploy:deploy-file -Dfile=hy.common.ui.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/ui/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/repository/thirdparty
+mvn deploy:deploy-file -Dfile=hy.common.ui-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/ui/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/repository/thirdparty
--- Maven.Deploy.Win.UI.bat
+++ Maven.Deploy.Win.UI.bat
@@ -1,2 +1,2 @@
-start mvn deploy:deploy-file -Dfile=hy.common.ui.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/ui/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:9015/nexus/content/repositories/thirdparty
-start mvn deploy:deploy-file -Dfile=hy.common.ui-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/ui/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:9015/nexus/content/repositories/thirdparty
+start mvn deploy:deploy-file -Dfile=hy.common.ui.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/ui/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/repository/thirdparty
+start mvn deploy:deploy-file -Dfile=hy.common.ui-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/ui/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/repository/thirdparty
